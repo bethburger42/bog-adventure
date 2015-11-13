08110055168cea4d9678c216029405f520e0b920
Beth Ernsberger <bether.ga.2015@gmail.com>
2015-11-13 10:19:23 -0800

    w07d03 bog adv ruby hw

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,41 @@
+class CreaturesController < ApplicationController
+def index
+	@creatures = Creature.all
+end
+
+def new
+	@creature = Creature.new
+end
+
+#post to db route not view route
+def create
+	Creature.create creature_params
+    redirect_to creatures_path
+end
+
+def show
+	@creature = Creature.find params[:id]
+end
+
+def edit
+	@creature = Creature.find params[:id]
+end
+
+def update
+	creature = Creature.find params[:id]
+    creature.update creature_params
+    redirect_to creatures_path
+end
+
+def destroy
+	Creature.find(params[:id]).delete
+    redirect_to creatures_path
+end
+
+private
+
+def creature_params
+	params.require(:creature).permit(:name, :description)
+end
+
+end
